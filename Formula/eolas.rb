d8cb2853bee8ba0a16babf8250c45ca905434eb0
Eolas Build Pipeline <build@eolas.co>
2026-06-16 14:05:02 -0400

    eolas 0.1.12485

--- Formula/eolas.rb
+++ Formula/eolas.rb
@@ -5,9 +5,9 @@
 class Eolas < Formula
   desc "Eolas CLI - terminal access to the Eolas platform API"
   homepage "https://github.com/eolas-hq/homebrew-eolas-cli"
-  version "0.1.12371"
-  url "https://github.com/eolas-hq/homebrew-eolas-cli/releases/download/v0.1.12371/eolas-0.1.12371-osx-arm64.tar.gz"
-  sha256 "19de83a64788b2691ef098369bd47464ddd4d6c49d48d96aba39c1038612222b"
+  version "0.1.12485"
+  url "https://github.com/eolas-hq/homebrew-eolas-cli/releases/download/v0.1.12485/eolas-0.1.12485-osx-arm64.tar.gz"
+  sha256 "671ec95e0c6495481ddd089cd440118f9f1c4def006777f3d415db12ab57b3be"
 
   depends_on :macos
   depends_on arch: :arm64
